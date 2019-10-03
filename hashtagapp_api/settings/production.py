7af87f6192859d435c62caf13c2211ccf8aef1ee
prasanta <prasantakakati1994@gmail.com>
2019-10-03 17:45:02 +0530

    added sentry integration

--- hashtagapp_api/settings/production.py
+++ hashtagapp_api/settings/production.py
@@ -1,8 +1,13 @@
 from .base import *
+import raven
 
 DEBUG = False
 
 ALLOWED_HOSTS = [get_env_var('HOST')]
+
+INSTALLED_APPS += [
+    'raven.contrib.django.raven_compat',
+]
 
 DATABASES = {
     'default': {
@@ -33,3 +38,55 @@ CSRF_COOKIE_SECURE = True
 X_FRAME_OPTIONS = 'DENY'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Sentry
+RAVEN_CONFIG = {
+    'dsn': get_env_var('SENTRY_DSN')
+}
+
+# Logging
+# Logging will be handled by sentry
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'ERROR',
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+            'tags': {'custom-tag': 'x'},
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+}
--- hashtagapp_api/wsgi.py
+++ hashtagapp_api/wsgi.py
@@ -13,5 +13,9 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hashtagapp_api.settings')
 
-# TODO: Add sentry for production
+SENTRY_ENABLED_IN = ['production', 'beta']
 application = get_wsgi_application()
+
+if os.environ.get('SITE_TYPE') in SENTRY_ENABLED_IN:
+    from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+    application = Sentry(get_wsgi_application())
